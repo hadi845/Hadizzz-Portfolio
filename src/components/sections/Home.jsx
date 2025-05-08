4012302f34681009bc424a2c94ee6a8ff3255166
Hadi Muhammad <hadimuhammad121@gmail.com>
2025-05-08 21:29:48 +0500

    Update files

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,5 +1,5 @@
 import { RevealOnScroll } from "../RevealOnScroll";
-import hadiImg from "../assets/me1.JPG"; // adjust the path based on your file structure
+import hadiImg from "../assets/me1.jpg"; // adjust the path based on your file structure
 
 
 export const Home = () => {
